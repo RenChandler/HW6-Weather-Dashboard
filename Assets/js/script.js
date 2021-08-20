06110e2a2bd696443eddc974d3ce7d8361d9b023
Ren <rnchandramouli42@gmail.com>
2021-08-20 14:01:30 -0600

    Added forecast function, call is working

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,6 +3,8 @@ var apiKey = "&appid=18be08014d8d2f2d7af221e103531551"
 
 var cityInput = document.querySelector("#city-search")
 var searchBtn = document.querySelector("#search-id")
+var date = moment().format("MMM Do, YYYY"); 
+
 
 
 
@@ -16,6 +18,7 @@ function getCity (city){
             }
         ).then(function (data){
             getToday(data[0].lat, data[0].lon)
+            getForecast(data[0].lat, data[0].lon)
         })
 
 }
@@ -46,7 +49,8 @@ function getToday(lat, lon){
     const humidity = document.createElement("p")
     const UV = document.createElement("p")
     const column = document.createElement("div")
-
+    
+    
     //Add Classes to DOM variables
     column.setAttribute("class","col-md")
     card.setAttribute("class","card")
@@ -56,23 +60,76 @@ function getToday(lat, lon){
     wind.setAttribute("class","card-text")
     humidity.setAttribute("class","card-text")
     UV.setAttribute("class","card-text")
+    //weatherIcon.setAttribute("src",iconImg)
+    
+    column.append(card)
+    card.append(cardBody)
+    cardBody.append(cardTitle,weatherIcon,temp,wind,humidity,UV)
+    
+    
+    
+    var todayURL =`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}${apiKey}&units=imperial`;
+    fetch(todayURL)
+    .then(
+        function(response){
+            return response.json()
+        }
+        ).then(function(data){
+            console.log(data)
+            //const iconImg = `https://openweathermap.org/img/w/${data.current.weather[0].icon}.png`
+            cardTitle.textcontent = `${cityInput.value} ${date} ` 
+            temp.textContent = `temp: ${data.current.temp}`
+            wind.textContent = `wind: ${data.current.wind_speed}`
+            humidity.textContent = `humidity: ${data.current.humidity}`
+            UV.textContent = `UV: ${data.current.uvi}`
+        })
+        todayEl.innerHTML= " "
+        todayEl.append(column)
+}
 
+function getForecast(lat, lon){
+    const forecastEl = document.querySelector(".forecast")
+    const card = document.createElement("div")
+    const cardBody = document.createElement("div")
+    const cardTitle = document.createElement("h2")
+    const weatherIcon = document.createElement("img")
+    const temp = document.createElement("p")
+    const wind = document.createElement("p")
+    const humidity = document.createElement("p")
+    const column = document.createElement("div")
+    
 
+    column.setAttribute("class","col-md")
+    card.setAttribute("class","card")
+    cardBody.setAttribute("class","card-body")
+    cardTitle.setAttribute("class","card-title")
+    temp.setAttribute("class","card-text")
+    wind.setAttribute("class","card-text")
+    humidity.setAttribute("class","card-text")
+    
+    column.append(card)
     card.append(cardBody)
     cardBody.append(cardTitle,temp,wind,humidity)
 
-    //var iconImg = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
-
-    var todayURL =`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}${apiKey}&units=imperial`;
-    fetch(todayURL)
-        .then(
-            function(response){
-                return response.json()
-            }
+    var forecastURL =`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}${apiKey}&units=imperial`;
+    fetch(forecastURL)
+    .then(
+        function(response){
+            return response.json()
+        }
         ).then(function(data){
-            console.log(data.current.temp)
-            temp.textContent = `temp: ${data.current.temp}`
-            wind.textContent = `temp: ${data.current.wind_speed}`
-            humidity.textContent = `temp: ${data.current.humidity}`
+            console.log(data.daily)
+
+
         })
-}
+    // for (let i=1; i<data.daily.length; i += 5) {
+    //     cardTitle.textcontent = `${cityInput.value} ${date} ` 
+    //     temp.textContent = `temp: ${data.daily[i].temp}`
+    //     wind.textContent = `wind: ${data.daily[i].wind_speed}`
+    //     humidity.textContent = `humidity: ${data.daily[i].humidity}`
+    //     forecastEl.innerHTML= " "
+    //     forecastEl.append(column)
+    //     return forecastEl
+
+
+    }
